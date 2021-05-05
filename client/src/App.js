c69ecd1218576f19c2559877b62102aabfabcbb4
Raghav Sharma <raghav13052001@gmail.com>
2021-05-05 17:05:02 +0530

    debugging 1

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const socket = io();
 const App = () => {
+  console.log('changed')
   return (
     <Router>
       <Switch>
       <Route
           path="/chatpage"
-          exact
           render={props=><ChatPage {...props} socket={socket} />}
       />
       <Route path="/" exact component={Home} />
--- client/src/components/chatPage.js
+++ client/src/components/chatPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {useHistory} from 'react-router-dom';
 import UserWindow from './userWindow';
 import MessageWindow from './messageWindow';
